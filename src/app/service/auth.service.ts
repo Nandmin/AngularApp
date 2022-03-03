f26ccfc53bf8aea2c74e92dced85aca88a9263f8
Nandmin <andras78.nemeth@gmail.com>
2022-03-03 20:53:12 +0100

    Auth service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@angular/core';
 import { ConfigService } from './config.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../model/user';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  loginUrl= `${this.config.apiUrl}login`;
+  logInUrl= `${this.config.apiUrl}login`;
+  logOutUrl= `${this.config.apiUrl}logout`;
+  //currentUserSubject: User  = new User();
+  currentUserSubject: BehaviorSubject<any> = new BehaviorSubject(null);
+  lastToken: string = ''; //user azonosítására szolgál
 
   constructor(
-    private config:ConfigService,
-    private http: HttpClientModule
+    private config: ConfigService,
+    private http: HttpClient,
+    private router: Router
+
   ) { }
+
+  get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  logOut(): void{
+    localStorage.removeItem('currentuser');
+    this.currentUserSubject.next(null)
+    this.router.navigate(['login']);
+  }
 }
