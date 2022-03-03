1f43cc8ab0c2611fb234eec55a9902f005e3600a
Nandmin <andras78.nemeth@gmail.com>
2022-03-03 23:20:41 +0100

    auth service changes

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { ConfigService } from './config.service';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { User } from '../model/user';
 import { Router } from '@angular/router';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,23 +14,51 @@ export class AuthService {
 
   logInUrl= `${this.config.apiUrl}login`;
   logOutUrl= `${this.config.apiUrl}logout`;
+  storageName = 'currentUser';
   //currentUserSubject: User  = new User();
-  currentUserSubject: BehaviorSubject<any> = new BehaviorSubject(null);
+  currentUserSubject: BehaviorSubject<any | null> = new BehaviorSubject(null);
   lastToken: string = ''; //user azonosítására szolgál
 
   constructor(
     private config: ConfigService,
     private http: HttpClient,
-    private router: Router
-
+    private router: Router,
+    private userService: UserService
   ) { }
 
   get currentUserValue(): User {
     return this.currentUserSubject.value;
   }
+  // sikeres login esetén, a szerver visszaad egy tokent, amit le kell tárolni a user azonosításhoz
+  login(loginData: User): Observable<{ accessToken: string }> {
+    return this.http.post<{ accesToken: string }>(
+      this.logInUrl,
+      { email: loginData.email, password: loginData.password }
+      )
+      .pipe( switchMap( response => {
+        if(response.accesToken){
+          this.lastToken = response.accesToken;
+          return this.userService.query(`email=${loginData.email}`);
+        }
+        return of(null);
+      }))
+      .pipe(
+        tap( user => {
+          if (!user){
+            localStorage.removeItem(this.storageName);
+            this.currentUserSubject.next(null);
+          }
+          else {
+            user[0].token = this.lastToken;
+            localStorage.setItem(this.storageName, JSON.stringify(user[0]));
+            this.currentUserSubject.next(user[0]);
+          }
+        })
+      );
+  }
 
   logOut(): void{
-    localStorage.removeItem('currentuser');
+    localStorage.removeItem(this.storageName);
     this.currentUserSubject.next(null)
     this.router.navigate(['login']);
   }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from './config.service';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
+
+//user adatok lekérdezése
 export class UserService {
 
-  constructor() { }
+  entity = 'users';
+
+  constructor(
+    private http: HttpClient,
+    private config: ConfigService
+  ) { }
+
+  get(id?: string | number):Observable<User | User[]>{
+    let url = `${this.config.apiUrl}${this.entity}`;
+
+    if(id) {
+      url += `${id}`
+    }
+
+    return this.http.get<User[]>(url);
+  }
+
+  query(querystring: string): Observable<User | User[]> {
+    const url = `${this.config.apiUrl}${this.entity}?${querystring}`;
+    return this.http.get<User[]>(url);
+  }
+
+  update(user: User): Observable<User> {
+    const url = `${this.config.apiUrl}${this.entity}?${user.id}`;
+    return this.http.put<User>(url, user);
+  }
 }
